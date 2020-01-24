f7f928d261a77fce5a0c52adb1c89638f6181ec1
Mrs. Jekel <sharlane.hess87@gmail.com>
2020-01-24 16:21:43 -0700

    Page Title

--- src/components/auth/signin.js
+++ src/components/auth/signin.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
 import SignInForm from './signinForm';
-
+import PageTitle from '../pageTitle';
 
  class SignIn extends Component {
 
@@ -12,6 +12,7 @@ import SignInForm from './signinForm';
   render() {
       return (
           <div className='sign-in'>
+              <PageTitle className='sign-in__page-title' title='Login' />
               <SignInForm onSubmit={this.onSubmit} className='sign-in__form' />
           </div>
       )
