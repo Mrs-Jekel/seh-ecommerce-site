270668a430784771000aa1e48537ffb710065798
Mrs. Jekel <sharlane.hess87@gmail.com>
2020-01-24 14:58:12 -0700

    sign in and up Routes

---
+++ src/components/auth/signin.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+ class SignIn extends Component {
+  constructor(props) {
+      super(props)
+
+      this.state = {
+          
+      }
+  }
+
+  render() {
+      return (
+          <div className='sign-in'>
+              sign in
+          </div>
+      )
+  }
+}
+export default SignIn;
