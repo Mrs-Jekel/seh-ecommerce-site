d692ea07d110eef849933c7b1c3b6c3f7574aeb3
Mrs. Jekel <sharlane.hess87@gmail.com>
2020-01-24 14:34:58 -0700

    layout grid

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import Header from "../components/headernav/header"
+import Navbar from "../components/headernav/navbar"
 
 class Layout extends Component {
   render() {
     return (
-      <div>
-       
+      <div className="layout">
         {this.props.children}
+        <Header />
+        <Navbar />
         
       </div>
     );
